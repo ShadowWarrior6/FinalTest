cefa29a085e20dbf14901f00ec2ea1247bb3a70f
Andranik <andran1401@gmail.com>
2023-05-13 20:56:23 +0400

    структура

--- final/Test/Program.cs
+++ final/Test/Program.cs
@@ -5,3 +5,15 @@
 
 string [] array_1 = new string[4]{"hello"  , "2" ,"world", "yess"};
 string [] array_2 = new string [array_1.Length];
+void Creating (string[] array_1, string[] array_2)
+{
+    int count = 0;
+    for (int i = 0; i < array_1.Length; i++)
+    {
+        if (array_1[i].Length<=3)
+        {
+            array_2[i]=array_1[i];
+            count++;
+        }
+    }
+}
